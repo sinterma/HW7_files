06401ab44d8e354499d53735c0c19b897214e6f2
Fedotovskaia Kseniia <sinterma@gmail.com>
2024-08-21 17:19:09 +0200

    new teste

---
+++ script_os.py
@@ -0,0 +1,9 @@
+import os.path
+
+CURRENT_FILE = os.path.abspath(__file__)
+
+CURRENT_DIR = os.path.dirname(CURRENT_FILE)
+print(CURRENT_DIR)
+
+TMP_DIR = os.path.join(CURRENT_DIR, "tmp")
+print(TMP_DIR)
