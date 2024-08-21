35a33989a73639d288a32ba1d2d8a02e9f475817
Fedotovskaia Kseniia <sinterma@gmail.com>
2024-08-21 19:39:55 +0200

    new teste

--- conftest.py
+++ conftest.py
@@ -1,17 +1,16 @@
-import os
+import pytest
 import zipfile
+import os
 
+@pytest.fixture(scope="function", autouse=True)
+def create_zip_archive():
+    def _create_zip_archive(zip_filename, files):
+        if os.path.exists(zip_filename):
+            os.remove(zip_filename)
 
-def create_zip_archive(zip_filename, files):
-    if os.path.exists(zip_filename):
-        os.remove(zip_filename)
+        os.makedirs(os.path.dirname(zip_filename), exist_ok=True)
 
-    os.makedirs(os.path.dirname(zip_filename), exist_ok=True)
-
-    with zipfile.ZipFile(zip_filename, 'w') as zip_file:
-        for file in files:
-            zip_file.write(file, arcname=os.path.basename(file))
-
-
-zip_filename = "tmp/archive.zip"
-files_to_zip = ["tmp/test.pdf", "tmp/train.csv", "tmp/import_empl_xlsx.xlsx"]
+        with zipfile.ZipFile(zip_filename, 'w') as zip_file:
+            for file in files:
+                zip_file.write(file, arcname=os.path.basename(file))
+    return _create_zip_archive
--- test_files.py
+++ test_files.py
@@ -1,24 +1,25 @@
-from pypdf import PdfReader
+import pytest
 import zipfile
+from pypdf import PdfReader
 import csv
 import io
 from openpyxl import load_workbook
 from io import BytesIO
 
-from conftest import create_zip_archive, zip_filename, files_to_zip
+# Определите ваши пути к файлам
+zip_filename = "tmp/archive.zip"
+files_to_zip = ["tmp/test.pdf", "tmp/train.csv", "tmp/import_empl_xlsx.xlsx"]
 
-create_zip_archive(zip_filename, files_to_zip)
-
-
-def test_pdf_zip():
+def test_pdf_zip(create_zip_archive):
+    create_zip_archive(zip_filename, files_to_zip)
     with zipfile.ZipFile(zip_filename, 'r') as zip_file:
         with zip_file.open("test.pdf") as pdf_file:
             reader = PdfReader(pdf_file)
             page_text = reader.pages[0].extract_text()
             assert "Тестовый PDF файл" in page_text
 
-
-def test_csv_zip():
+def test_csv_zip(create_zip_archive):
+    create_zip_archive(zip_filename, files_to_zip)
     with zipfile.ZipFile(zip_filename, 'r') as zip_file:
         with zip_file.open("train.csv") as csv_file:
             csv_content = csv_file.read()
@@ -28,8 +29,8 @@ def test_csv_zip():
             header = next(reader)
             assert "Name" in header
 
-
-def test_xlsx_zip():
+def test_xlsx_zip(create_zip_archive):
+    create_zip_archive(zip_filename, files_to_zip)
     with zipfile.ZipFile(zip_filename, 'r') as zip_file:
         with zip_file.open("import_empl_xlsx.xlsx") as xlsx_file:
             with BytesIO(xlsx_file.read()) as temp_file:
@@ -37,8 +38,3 @@ def test_xlsx_zip():
                 sheet = workbook.active
                 text = sheet.cell(row=2, column=2).value
                 assert "Иванова" in text
-
-
-test_pdf_zip()
-test_csv_zip()
-test_xlsx_zip()
